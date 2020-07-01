171281d7ae5319c120906ebbc021b28e92bd3265
Leonardo Garruba <leonardogarrubaaraujo@gamil.com>
2020-07-01 17:32:48 -0300

    Conexão com banco dde dados via Knex

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,22 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+    client: 'pg',
 
-  staging: {
-    client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: process.env.PG_DB_HOST,
+      user: process.env.PG_DB_USER,
+      password: process.env.PG_DB_PASSWORD,
+      database: process.env.PG_DB_NAME,
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      directory: './src/database/migrations'
+    },
+
+  },
 
 };
