c6826fbee050e934c9eb4170562903c8f4feb4e3
Leonardo Garruba <leonardogarrubaaraujo@gamil.com>
2020-07-01 22:10:29 -0300

    Configurada rota de busca das prevenções

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
+const Precautions = require('./controllers/PrecationsController');
 
+router.get('/precautions', Precautions.index);
 
 module.exports = router;
